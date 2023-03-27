c647ddc872cebb73a9c62095f37ec4d47bb6a0e0
EGLR <117935328+S133025828besd@users.noreply.github.com>
2023-03-27 14:39:32 -0600

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,55 @@
-let Bird_Location = 0
-let random_bar = 0
+function BirdLocation () {
+    if (Bird_Location == 1) {
+        basic.showLeds(`
+            # . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (Bird_Location == 2) {
+        basic.showLeds(`
+            . . . . .
+            # . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (Bird_Location == 3) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (Bird_Location == 4) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . .
+            . . . . .
+            `)
+    }
+    if (Bird_Location == 5) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . .
+            `)
+    }
+}
 input.onButtonPressed(Button.A, function () {
-    Bird_Location += -1
+    Bird_Location += 1
 })
 input.onButtonPressed(Button.B, function () {
-    Bird_Location += 1
+    Bird_Location += -1
 })
 function Generate_Pipe () {
     random_bar = randint(1, 5)
@@ -54,6 +99,9 @@ function Generate_Pipe () {
             `)
     }
 }
+let random_bar = 0
+let Bird_Location = 0
+Bird_Location = 3
 basic.forever(function () {
-	
+    BirdLocation()
 })
